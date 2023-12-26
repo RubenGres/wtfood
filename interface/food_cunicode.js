71304d16ba6eaa854597df0e9922f9cb0a4e6b31
Retronyme <ruben.gres@proton.me>
2023-12-26 09:23:37 +0000

    changed loading

--- interface/food_cunicode.js
+++ interface/food_cunicode.js
@@ -34,12 +34,19 @@ async function generateImage(id_image) {
     
     div.innerHTML = `
         <div class="imgWrap">
-            <img class="grayscale" draggable="false" src="image/food_class_${id_image+1}_sq.jpg">
-            <img class="red" draggable="false" src="image/food_class_${id_image+1}_sq.jpg">
-            <img class="blue" draggable="false" src="image/food_class_${id_image+1}_sq.jpg">
+            <div class="circle circle1 loading_${id_image+1}"></div>
+            <div class="circle circle2 loading_${id_image+1}"></div>
+            <div class="circle circle3 loading_${id_image+1}"></div>
         </div>
     `;
-    div.style.backgroundImage = "";
+
+    var elements = document.getElementsByClassName(`loading_${id_image+1}`);
+    
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundImage = `url('image/food_class_${id_image+1}_sq.jpg')`;
+    }
+
+    div.style.backgroundImage = `image/food_class_${id_image+1}_sq.jpg`;
 
     const data = {
         image_url: '/home/ruben/food-dysmorphia/no_canvas/image/food_class_'+(id_image+1)+'_sq.jpg',
