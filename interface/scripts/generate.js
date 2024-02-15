42f69b5a659baecd941f260984a09ed15cc007c8
Retronyme <ruben.gres@proton.me>
2024-02-15 13:38:25 +0000

    Added cell animation for mobile

--- interface/scripts/generate.js
+++ interface/scripts/generate.js
@@ -3,22 +3,6 @@ async function generateImage(id_image) {
 
     let cell_src = div.firstElementChild.src;
 
-    div.innerHTML = `
-        <div class="imgWrap">
-            <div class="circle circle1 loading_${id_image+1}"></div>
-            <div class="circle circle2 loading_${id_image+1}"></div>
-            <div class="circle circle3 loading_${id_image+1}"></div>
-        </div>
-    `;
-
-    var elements = document.getElementsByClassName(`loading_${id_image+1}`);
-    
-    for(var i = 0; i < elements.length; i++) {
-        elements[i].style.backgroundImage = `url(${cell_src})`;
-    }
-
-    div.style.backgroundImage = `image/food_class_${id_image+1}_sq.jpg`;
-
     const data = {
         image_url: '/home/ruben/food-dysmorphia/no_canvas/image/food_class_'+(id_image+1)+'_sq.jpg',
         prompt: "candies, sugar, sweets"
