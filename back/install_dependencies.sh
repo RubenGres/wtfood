576ab197e61cd1a27dd5e6de90900b9ba76011be
Retronyme <ruben.gres@proton.me>
2024-03-21 10:44:01 +0000

    updated requirements

--- back/install_dependencies.sh
+++ back/install_dependencies.sh
@@ -21,6 +21,3 @@ git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts
 git clone https://github.com/Fannovel16/comfyui_controlnet_aux
 git clone https://github.com/Fannovel16/ComfyUI-Frame-Interpolation
 git clone https://github.com/Fannovel16/comfyui_controlnet_aux
-
-pip3 install imageio-ffmpeg
-pip3 install opencv-python
