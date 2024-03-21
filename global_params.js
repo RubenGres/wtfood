ec79fb0b7f73dd24c48422a993f4f032b067f0f6
RubenGres <rubengres@hotmail.fr>
2024-03-21 13:14:38 +0000

    Deploying to gh-pages from @ RubenGres/food-dysmorphia@46fa47f87cfddcf27efc0e433dc49a596197c5d1 🚀

--- global_params.js
+++ global_params.js
@@ -2,14 +2,7 @@ const SD_API_URL = "https://toa4nqj3umza2o-5000.proxy.runpod.net/";
 
 let cell_w = 128;
 let cell_h = 128;
+
 const cell_margin = 10;
 
 const camera_output_size = 512;
-
-const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-if(isMobile) {
-    const shorterSide = Math.min(window.visualViewport.width, window.visualViewport.height);
-    const mobileSize = Math.round(shorterSide * 0.85);
-    cell_w = mobileSize;
-    cell_h = mobileSize;
-}
--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,13 @@
+// check if we are on a mobile phone
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if(isMobile) {
+    const shorterSide = Math.min(window.visualViewport.width, window.visualViewport.height);
+    const mobileSize = Math.round(shorterSide * 0.85);
+    cell_w = mobileSize;
+    cell_h = mobileSize;
+}
+
+// global cell list that will contain all the cards
 let cells = [];
 
 var container = document.getElementById('foodmap');
