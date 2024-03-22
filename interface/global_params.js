01b34f0e93700a4481d850c15d40b57ef401119e
Ruben Gres <ruben.gres@proton.me>
2024-03-22 00:41:42 +0000

    added zoom in control panel

--- interface/global_params.js
+++ interface/global_params.js
@@ -1,8 +1,9 @@
 const SD_API_URL = "https://toa4nqj3umza2o-5000.proxy.runpod.net/";
 
+let zoom = 1;
+
 let cell_w = 128;
 let cell_h = 128;
-
-const cell_margin = 10;
+let cell_margin = 10;
 
 const camera_output_size = 512;
