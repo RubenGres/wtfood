3c27b63dcf5a3515f5469092943de67cc67bd8e9
Ruben Gres <ruben.gres@proton.me>
2024-03-22 00:41:56 +0000

    renamed css class

--- interface/js/card.js
+++ interface/js/card.js
@@ -1,7 +1,7 @@
 function card_content(media_src, cardtext) {
     // Create image element
     const generated_card = document.createElement('div');
-    generated_card.setAttribute('class', 'generated_card')
+    generated_card.setAttribute('class', 'generated-card')
 
     // Check if media_src is an image (ends with .jpg, .png, etc.)
     if (/\.(jpg|jpeg|png|gif)$/i.test(media_src)) {
@@ -15,7 +15,7 @@ function card_content(media_src, cardtext) {
         const video = document.createElement('video');
         video.setAttribute('class', 'generated-media');
         video.setAttribute('loop', '');
-        video.setAttribute('autoplay', '');
+        //video.setAttribute('autoplay', '');
         video.setAttribute('muted', ''); // Muted attribute to allow autoplay in most browsers
         const source = document.createElement('source');
         source.src = media_src;
@@ -32,9 +32,22 @@ function card_content(media_src, cardtext) {
     generated_card.appendChild(infotext);
 
     // Add click event listener to the image
-    generated_card.addEventListener('click', function() {
+    generated_card.addEventListener('click', async function() {
         infotext.style.display = infotext.style.display === 'none' ? 'block' : 'none';
     });
+
+    // Check if generated_card has a video child, then play/pause the video on hover
+    if (generated_card.querySelector('video')) {
+        generated_card.addEventListener('mouseenter', function() {
+            // Play video
+            this.querySelector('video').play();
+        });
+
+        generated_card.addEventListener('mouseleave', function() {
+            // Pause video
+            this.querySelector('video').pause();
+        });
+    }
 
     return generated_card;
 }
