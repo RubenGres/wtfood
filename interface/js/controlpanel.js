68b8491d7022b6050232238a04c4233ec531f3f9
Retronyme <ruben.gres@proton.me>
2024-04-03 14:14:41 +0000

    different control shceme to get rid of the zoom slider

--- interface/js/controlpanel.js
+++ interface/js/controlpanel.js
@@ -11,12 +11,6 @@ document.querySelectorAll('input[name="sortOption"]').forEach((elem) => {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    var zoomSlider = document.getElementById('zoomSlider');
-    zoomSlider.oninput = function() {
-        zoom = this.value;
-        updateDivPositions();
-    };
-
     var okButton = document.getElementById('okButton');
     var xLabelInput = document.getElementById('xLabelInput');
     var yLabelInput = document.getElementById('yLabelInput');
