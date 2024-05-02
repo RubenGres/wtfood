b2beda3c92ecc555b310de680b7d9de5c438af01
Retronyme <ruben.gres@proton.me>
2024-05-02 20:55:06 +0000

    card zoom level computed in setup

--- interface/global_params.js
+++ interface/global_params.js
@@ -8,7 +8,8 @@ let cell_margin = 10;
 
 const camera_output_size = 512;
 
-const CARD_ZOOM_LEVEL = 0.9 * Math.min(window.innerWidth, window.innerHeight) / (cell_w + cell_margin);
+const CARD_ZOOM_LEVEL = 0.8
+const ZOOM_DURATION_MS = 300;
 const MOBILE_MIN_ZOOM = 0.05;
 const SWIPE_MAX_DURATION_MS = 300;
 const MIN_SWIPE_DIST = 30;
