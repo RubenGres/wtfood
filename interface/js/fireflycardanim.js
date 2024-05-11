8ab52a65799f1a165c3b2f77a11fe417906053dc
Retronyme <ruben.gres@proton.me>
2024-05-11 16:31:41 +0000

    fixed fireflies animation selection

--- interface/js/fireflycardanim.js
+++ interface/js/fireflycardanim.js
@@ -1,6 +1,7 @@
 // Function to pick 5 random unique elements from the list
 function pickRandomCards(count) {
-    const cards_pool = Object.values(cells);
+    let cards_pool = Object.values(cells);
+    cards_pool = cards_pool.filter(cell => typeof cell.id === 'number');
 
     const shuffled = cards_pool.slice(); // Copy to avoid mutating the original array
     for (let i = cards_pool.length - 1; i > 0; i--) {
@@ -14,6 +15,7 @@ function pickRandomCards(count) {
 async function loopFireflyAnimation() {
     while (true) {
         const selectedElements = pickRandomCards(FIREFLY_AMOUNT);
+        console.log(selectedElements);
         selectedElements.forEach((card) => play_video(card["elem"]));
         await new Promise(resolve => setTimeout(resolve, FIREFLY_ANIMATION_TIME_MS));
         selectedElements.forEach((card) => pause_other_videos(card["elem"]));
